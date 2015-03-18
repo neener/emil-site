05282fe26a80528337024fac81930a058d08946a
Janine Rosen <janinemrosen@gmail.com>
2015-03-17 22:13:45 -0400

    adjusted arrows, resized for phone

--- slideshow.js
+++ slideshow.js
@@ -29,35 +29,3 @@ $(document).ready(function(){
 });
 
 
-
-
-
-
-// $(document).ready(function () {
-//     setInterval(getNext, 2000); // milliseconds
-//     $('#next').on('click', getNext);
-//     $('#prev').on('click', getPrev);
-// });
-
-// function getNext() {
-//     var $curr = $('#artwork div.current'),
-//         $next = ($curr.next().length) ? $curr.next() : $('#artwork div').first();
-
-//     transition($curr, $next);
-// }
-
-// function getPrev() {
-//     var $curr = $('#artwork div.current'),
-//         $next = ($curr.prev().length) ? $curr.prev() : $('#artwork div').last();
-//     transition($curr, $next);
-// }
-
-// function transition($curr, $next) {
-//     clearInterval(interval);
-
-//     $next.css('z-index', 2).fadeIn('slow', function () {
-//         $curr.hide().css('z-index', 0);
-//         $next.css('z-index', 1);
-//     });
-
-// }
